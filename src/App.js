6d282ae12ab863078b25bb1a4a782f9189d34bba
YacqubAdan <yacquubadan.1@gmail.com>
2022-05-26 00:04:06 +0100

    added all film rows

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ function App() {
       <Row
         title="NETFLIX ORIGINALS"
         fetchURL={requests.fetchNetflixOriginals}
-      ></Row>
-      <Row title="Trending Now" fetchURL={requests.fetchTrending}></Row>
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romancce Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
