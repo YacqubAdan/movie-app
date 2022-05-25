ce2d66257bf5c355a0607bf63d0a0e148edbd8f5
YacqubAdan <yacquubadan.1@gmail.com>
2022-05-25 19:08:09 +0100

    added row components in app with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Row from "./components/Row";
+import requests from "./api/requests";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+      ></Row>
+      <Row title="Trending Now" fetchURL={requests.fetchTrending}></Row>
+    </div>
+  );
 }
 
 export default App;
