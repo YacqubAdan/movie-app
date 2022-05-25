732c59e6936083ced5d6faaf79bc5b65cfb824c5
YacqubAdan <yacquubadan.1@gmail.com>
2022-05-26 00:07:08 +0100

    added more tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".content-auto": {
+          "content-visibility": "auto",
+        },
+        ".content-hidden": {
+          "content-visibility": "hidden",
+        },
+        ".content-visible": {
+          "content-visibility": "visible",
+        },
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+      });
+    }),
+  ],
 };
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+// .no-scrollbar::-webkit-scrollbar {
+//   display: none;
+// }
+
+// /* Hide scrollbar for IE, Edge and Firefox */
+// .no-scrollbar {
+//   -ms-overflow-style: none;  /* IE and Edge */
+//   scrollbar-width: none;  /* Firefox */
+// }
