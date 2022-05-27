5551a3b0dfec86391c0123a62477887397a75223
YacqubAdan <yacquubadan.1@gmail.com>
2022-05-27 16:42:30 +0100

    added useEffect hook that listens to scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="fixed top-0  flex justify-between w-full p-5 bg-stone-900">
+    <div
+      className={`fixed top-0 z-10 transition-all duration-500 ease-in flex justify-between w-full p-5 ${
+        show && "bg-stone-900"
+      }`}
+    >
       <img
         className="object-contain w-20"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
